fb9ead44f3e4e9760e31cbfdba2a7e25c8f0576c
Eric Lawrence <e_lawrence@hotmail.com>
2022-06-20 12:02:03 -0500

    v1.0.0.1
    Only change to the page on uninstall

--- background.js
+++ background.js
@@ -8,12 +8,13 @@ chrome.management.onUninstalled.addListener((e)=> {onExtUninstall(e); });
 chrome.management.onEnabled.addListener((e)=> {onExtEnable(e); });
 chrome.management.onDisabled.addListener((e)=> {onExtDisable(e); });
 
-// Record an Event in the log.
-function logEvent(sEvent) {
+// Record an Event in the log, then optionally call a function.
+function logEvent(sEvent, fnThen) {
     chrome.storage.local.get(null, function(info) {
         let sLog = info["sLog"] || "";
         sLog = new Date().toISOString() + ": " + sEvent + "\n" + info["sLog"];
         chrome.storage.local.set({"sLog":sLog}, null);
+        if (fnThen) fnThen();
     });  
 }
 
@@ -22,8 +23,7 @@ function onExtInstall(e) {
     //chrome.tabs.create({ url: "/monitor.html?op=Install&name=" + e.shortName + "&id=" + e.id });
 }
 function onExtUninstall(id) {
-    logEvent(`Uninstalled id=${id}.`);
-    chrome.tabs.create({ url: "/monitor.html?op=Uninstall&id=" + id });
+    logEvent(`Uninstalled id=${id}.`, ()=>{ chrome.tabs.create({ url: "/monitor.html?op=Uninstall&id=" + id }); });
 }
 function onExtEnable(e) {
     logEvent(`Enabled     id=${e.id},name=${e.shortName}.`);
@@ -33,10 +33,8 @@ function onExtDisable(e) {
     logEvent(`Disabled    id=${e.id},name=${e.shortName},reason=${e.disabledReason}.`);
     //chrome.tabs.create({ url: "/monitor.html?op=Disable&name=" + e.shortName + "&id=" + e.id + "&reason=" + e.disabledReason });
 }
-// chrome.browserAction.setBadgeText( {text: "dev"} );
 
-chrome.browserAction.onClicked.addListener (
-    (t)=>{
+chrome.browserAction.onClicked.addListener((t)=>{
       chrome.tabs.create({ url: "/monitor.html?op=ShowLog" });
     }
 );
